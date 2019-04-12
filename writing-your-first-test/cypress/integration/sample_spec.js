39602f2bef35f8525d60597431d64a4fc2e36d48
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-11 21:06:47 -0400

    cy visit implemented

--- writing-your-first-test/cypress/integration/sample_spec.js
+++ writing-your-first-test/cypress/integration/sample_spec.js
@@ -1,5 +1,12 @@
-describe("it does not do much", ()=>{
-    it("does not do much", ()=>{
-        expect(true).to.equal(true)
+describe("My First Test", ()=>{
+    it("Visits the kitchen sink", ()=>{
+        // arrange - set up initial app state
+        // visit a page
+        cy.visit("https://example.cypress.io")
+        // query to find an element
+        // act - take an action
+        // interact with that element
+        // make an assertion
+        // make an assertion about the changes in the app
     })
 })
